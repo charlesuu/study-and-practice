5a342408098284bb61aa1aaf2638c79037967311
charlesuu <tiger0192004@gmail.com>
2023-07-01 14:46:59 +0900

    no message

--- module1/src/main/java/org/example/Main.java
+++ module1/src/main/java/org/example/Main.java
@@ -1,56 +1,14 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        MyCollection<String> myCollection = new MyCollection<>(Arrays.asList("현호", "하하", "호호"));
 
-        int N = sc.nextInt();
-
-        int[] A = new int[N];
-        int[] tmpA = new int[N];
-
-        int[] B = new int[N];
-        boolean[] visitedB = new boolean[N];
-
-
-        for (int i = 0; i < N; i++) {
-            A[i] = sc.nextInt();
-        }
-        for (int i = 0; i < N; i++) {
-            B[i] = sc.nextInt();
-        }
-
-        Arrays.sort(A);
-
-        int aCount = 0;
-        for (int i = 0; i < N; i++) {
-            int max = -1;
-            int maxIndex = -1;
-
-            for (int j = 0; j < N; j++) {
-                if (B[j] > max && visitedB[j] == false) {
-                    visitedB[j] = true;
-
-                    if (maxIndex != -1) {
-                        visitedB[maxIndex] = false;
-                    }
-                    max = B[j];
-                    maxIndex = j;
-                }
-            }
-
-            tmpA[maxIndex] = A[aCount];
-            aCount++;
-        }
-
-        int answer = 0;
-        for (int i = 0; i < N; i++) {
-            answer += tmpA[i] * B[i];
-        }
-        System.out.println(answer);
+        myCollection.foreach(d -> System.out.println(d));
     }
 }
 
