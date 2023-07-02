d2e28c58c13020e07a3e440c3fe16205827f6625
charlesuu <tiger0192004@gmail.com>
2023-07-02 23:07:47 +0900

    알고리즘 문제 풀이

--- module1/src/main/java/org/example/Main.java
+++ module1/src/main/java/org/example/Main.java
@@ -1,14 +1,38 @@
 package org.example;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        MyCollection<String> myCollection = new MyCollection<>(Arrays.asList("현호", "하하", "호호"));
+        Scanner sc = new Scanner(System.in);
 
-        myCollection.foreach(d -> System.out.println(d));
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int count = sc.nextInt();
+
+        int max = 0;
+        for (int i = 0; i < count; i++) {
+            int num = sc.nextInt();
+            list.add(num);
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        int dp[] = new int[max + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for (int i = 4; i <= max; i++) {
+            dp[i] = dp[i - 3] +dp[i - 2] +dp[i - 1];
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            int num = list.get(i);
+            System.out.println(dp[num]);
+        }
     }
 }
 
