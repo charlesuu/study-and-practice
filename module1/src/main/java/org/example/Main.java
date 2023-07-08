d262fd31536f2edf4fdb24fb8fa99689f377186b
charlesuu <tiger0192004@gmail.com>
2023-07-08 23:55:20 +0900

    no message

--- module1/src/main/java/org/example/Main.java
+++ module1/src/main/java/org/example/Main.java
@@ -1,30 +1,32 @@
 package org.example;
+
+import java.util.Scanner;
+
 public class Main {
-
     public static void main(String[] args) {
-        long t = System.currentTimeMillis(); // 현재 시간(밀리초)를 저장
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
 
-        Long sum = 0L; // 래퍼 객체로 오토 박싱으로 정수 값을 저장
+        for (int i = 0; i < T; i++) {
+            int N = sc.nextInt();
+            int[] arr = new int[N];
 
-        // 백만번 도는 동안 더하기 연산
-        for (long i = 0; i < 1000000; i++) {
-            sum += i;
+            for (int j = 0; j < N; j++) {
+                arr[j] = sc.nextInt();
+            }
+
+
+            int max = -1;
+            long margin = 0;
+            for (int j = N - 1; j >= 0; j--) {
+                if (arr[j] > max) {
+                    max = arr[j];
+                } else {
+                    margin += max - arr[j];
+                }
+            }
+            System.out.println(margin);
         }
-
-        System.out.println("processing time: " + (System.currentTimeMillis() - t) + " ms") ;
-        //processing time: 34 ms
-
-
-//        long t = System.currentTimeMillis();
-//
-//        long sum = 0L; // 기본형 정수 타입인 long 자료형에 정수 저장
-//
-//        for (long i = 0; i < 1000000; i++) {
-//            sum += i;
-//        }
-//
-//        System.out.println("processing time: " + (System.currentTimeMillis() - t) + " ms") ;
-//        //processing time: 5 ms
     }
 }
 
