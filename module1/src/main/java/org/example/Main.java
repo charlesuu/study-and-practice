909e86774afe3a36425b2da1874bf7f3d3d267f2
charlesuu <tiger0192004@gmail.com>
2023-07-23 23:26:31 +0900

    no message

--- module1/src/main/java/org/example/Main.java
+++ module1/src/main/java/org/example/Main.java
@@ -3,29 +3,36 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String equation = reader.readLine();
-        String[] equationArr = equation.split("-");
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
 
-        int ret = 0;
+        int[] result = twoSum(nums, target);
 
-        for (int i = 0; i < equationArr.length; i++) {
-            int sum = 0;
-
-            String[] arr = equationArr[i].split("\\+");
-            for (String num : arr) {
-                sum += Integer.parseInt(num);
-            }
-            if (i == 0) {
-                ret += sum;
-            } else {
-                ret -= sum;
-            }
+        if (result != null) {
+            System.out.println("두 수의 합이 " + target + "인 원소의 인덱스: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("해당하는 조합이 없습니다.");
         }
-        System.out.println(ret);
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numToIndexMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numToIndexMap.containsKey(complement)) {
+                int[] result = {numToIndexMap.get(complement), i};
+                return result;
+            }
+            numToIndexMap.put(nums[i], i);
+        }
+
+        return null; // 해당하는 조합이 없는 경우
     }
 }
