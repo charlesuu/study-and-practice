30452ce198d6e89ac793114ee1873d3c61e2eccc
charlesuu <tiger0192004@gmail.com>
2023-08-03 12:54:06 +0900

    알고리즘 : 이진 변환 반복하기

--- module1/src/main/java/org/example/Main.java
+++ module1/src/main/java/org/example/Main.java
@@ -1,42 +1,32 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 
 public class Main {
     public static void main(String[] args) {
 
     }
-
-
-
     private int countZeros(String s) {
-         int zeros = 0;
-         for (char c : s.toCharArray()) {
-             if (c == '0') zeros++;
-         }
-         return zeros;
-     }
+        int zeros = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '0') zeros++;
+        }
+        return zeros;
+    }
 
-     public int[] solution(String s) {
-         int loop = 0;
-         int removed = 0;
+    public int[] solution(String s) {
+        int loop = 0;
+        int removed = 0;
 
-         while (!s.equals("1")) {
-             int zeros = countZeros(s);
-             loop += 1;
-             removed += zeros;
+        while (!s.equals("1")) {
+            int zeros = countZeros(s);
+            loop += 1;
 
-             int ones = s.length() - zeros;
-             s = Integer.toString(ones, 2);
-         }
+            removed += zeros;
 
-         return new int[] {loop, removed};
-     }
+            int ones = s.length() - zeros;
+            s = Integer.toString(ones, 2);
+        }
+
+        return new int[] {loop, removed};
+    }
 }
