64d3d4af677856f207c5f0a0b18b6694a6144ce3
charlesuu <tiger0192004@gmail.com>
2023-08-05 12:33:30 +0900

    알고리즘 : 쿼드 압축 후 수 세기

--- module1/src/main/java/org/example/Main.java
+++ module1/src/main/java/org/example/Main.java
@@ -2,31 +2,46 @@ package org.example;
 
 
 public class Main {
+    private static class Count {
+    public final int zero;
+    public final int one;
+
+    public Count(int zero, int one) {
+        this.zero = zero;
+        this.one = one;
+    }
+
+    public Count add(Count other) {
+        return new Count(zero + other.zero, one + other.one);
+    }
+}
+
+    private Count count(int offsetX, int offsetY, int size,
+            int[][] arr) {
+        int h = size / 2;
+        for (int x = offsetX; x < offsetX + size; x++) {
+            for (int y = offsetY; y < offsetY + size; y++) {
+                if (arr[y][x] != arr[offsetY][offsetX]) {
+                    return count(offsetX, offsetY, h, arr)
+                            .add(count(offsetX + h, offsetY, h, arr))
+                            .add(count(offsetX, offsetY + h, h, arr))
+                            .add(count(offsetX + h, offsetY + h, h, arr));
+                }
+            }
+        }
+
+        if (arr[offsetY][offsetX] == 1) {
+            return new Count(0, 1);
+        }
+        return new Count(1, 0);
+    }
+
+    public int[] solution(int[][] arr) {
+        Count count = count(0, 0, arr.length, arr);
+        return new int[] {count.zero, count.one};
+    }
+
     public static void main(String[] args) {
 
-    }
-    private int countZeros(String s) {
-        int zeros = 0;
-        for (char c : s.toCharArray()) {
-            if (c == '0') zeros++;
-        }
-        return zeros;
-    }
-
-    public int[] solution(String s) {
-        int loop = 0;
-        int removed = 0;
-
-        while (!s.equals("1")) {
-            int zeros = countZeros(s);
-            loop += 1;
-
-            removed += zeros;
-
-            int ones = s.length() - zeros;
-            s = Integer.toString(ones, 2);
-        }
-
-        return new int[] {loop, removed};
     }
 }
---
+++ module1/src/main/java/org/example/Order.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class Order {
+    private long cancelPrice;
+    private long orderPrice;
+}
