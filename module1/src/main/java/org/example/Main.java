440aef5a381709ab4c1fe10c5b795fed97ca4126
charlesuu <tiger0192004@gmail.com>
2023-08-06 22:19:15 +0900

    알고리즘

--- module1/src/main/java/org/example/Main.java
+++ module1/src/main/java/org/example/Main.java
@@ -1,47 +1,30 @@
 package org.example;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    private static class Count {
-    public final int zero;
-    public final int one;
-
-    public Count(int zero, int one) {
-        this.zero = zero;
-        this.one = one;
-    }
-
-    public Count add(Count other) {
-        return new Count(zero + other.zero, one + other.one);
-    }
-}
-
-    private Count count(int offsetX, int offsetY, int size,
-            int[][] arr) {
-        int h = size / 2;
-        for (int x = offsetX; x < offsetX + size; x++) {
-            for (int y = offsetY; y < offsetY + size; y++) {
-                if (arr[y][x] != arr[offsetY][offsetX]) {
-                    return count(offsetX, offsetY, h, arr)
-                            .add(count(offsetX + h, offsetY, h, arr))
-                            .add(count(offsetX, offsetY + h, h, arr))
-                            .add(count(offsetX + h, offsetY + h, h, arr));
-                }
-            }
-        }
-
-        if (arr[offsetY][offsetX] == 1) {
-            return new Count(0, 1);
-        }
-        return new Count(1, 0);
-    }
-
-    public int[] solution(int[][] arr) {
-        Count count = count(0, 0, arr.length, arr);
-        return new int[] {count.zero, count.one};
-    }
 
     public static void main(String[] args) {
 
+    }
+
+    private static final char[] CHARS = "AEIOU".toCharArray();
+
+    private List<String> generate(String word) {
+        List<String> words = new ArrayList<>();
+        words.add(word);
+
+        if (word.length() == 5) return words;
+
+        for (char c : CHARS) {
+            words.addAll(generate(word + c));
+        }
+        return words;
+    }
+
+    public int solution(String word) {
+        return generate("").indexOf(word);
     }
 }
