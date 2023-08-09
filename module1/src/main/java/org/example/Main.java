58825655cf07bff60eb6b9c395487de10948f538
charlesuu <tiger0192004@gmail.com>
2023-08-09 19:49:50 +0900

    알고리즘

--- module1/src/main/java/org/example/Main.java
+++ module1/src/main/java/org/example/Main.java
@@ -12,33 +12,23 @@ public class Main {
 
     }
 
-    private boolean isPrime(int n) {
-        if (n <= 1) return false;
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0) return false;
+    public int[] solution(int brown, int yellow) {
+        int[] answer = {};
+
+        for (int width = 3; width < 5000; width++) {
+            for (int height = 0; height <= width; height++) {
+                int brown_calc = (width + height - 2) * 2;
+                int yellow_calc = width * height - brown_calc;
+
+                if (brown_calc == brown && yellow_calc == yellow) {
+                    return new int[] {
+                            width,
+                            height
+                    };
+                }
+            }
         }
-        return true;
-    }
 
-    private void getPrimes(int acc, int[] numbers, boolean[] isUsed,
-            Set<Integer> primes) {
-        if (isPrime(acc)) primes.add(acc);
-
-        for (int i = 0; i < numbers.length; i++) {
-            if (isUsed[i]) continue;
-
-            int nextAcc = acc * 10 + numbers[i];
-
-            isUsed[i] = true;
-            getPrimes(nextAcc, numbers, isUsed, primes);
-            isUsed[i] = false;
-        }
-    }
-
-    public int solution(String nums) {
-        Set<Integer> primes = new HashSet<>();
-        int[] numbers = nums.chars().map(c -> c - '0').toArray();
-        getPrimes(0, numbers, new boolean[numbers.length], primes);
-        return primes.size();
+        return null;
     }
 }
