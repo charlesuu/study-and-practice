bf438c7dc5817c581c315500f5771d7c4ce1e70e
charlesuu <tiger0192004@gmail.com>
2023-08-11 21:22:51 +0900

    알고리즘

--- module1/src/main/java/org/example/Main.java
+++ module1/src/main/java/org/example/Main.java
@@ -1,86 +1,145 @@
 package org.example;
 
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.Deque;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Main {
 
+    private static final String operandExpression = "\\*|\\-|\\+";
+    private static final String operatorExpression = "[0-9]+";
+    private static final String[][] operatorPriority = {
+            {"*","+","-"},{"-","+","*"},{"*","-","+"},
+            {"+","-","*"},{"+","*","-"},{"-","*","+"}
+    };
     public static void main(String[] args) {
 
-    }
-    private static class Course {
-        public final String course;
-        public final int occurrences;
 
-        public Course(String course, int occurrences) {
-            this.course = course;
-            this.occurrences = occurrences;
+    }
+
+    public long solution(String expression) {
+        long answer = 0;
+        List<String> operands = getOperands(expression);
+        List<String> operators = getOperators(expression);
+        List<String> formula = getFormula(operands, operators);
+        List<Long> results = new ArrayList<>();
+
+        for(int i = 0; i < 6; i++){
+            List<String> postfix = toPostfix(formula, i);
+            long result = calculate(postfix);
+            result = Math.abs(result);
+            results.add(result);
         }
+        answer = results.stream().mapToLong(Long::longValue).max().getAsLong();
+        return answer;
     }
 
-    private void getCourses(char nextMenu, Set<String> selectedMenus,
-            List<Set<String>> orderList,
-            Map<Integer, List<Course>> courses) {
-        int occurrences = (int) orderList.stream()
-                .filter(order -> order.containsAll(selectedMenus))
-                .count();
-        if (occurrences < 2) return;
+    private List<String> getOperands(String expression){
+        String[] operands = expression.split(operandExpression);
 
-        int size = selectedMenus.size();
-        if (courses.containsKey(size)) {
-            List<Course> courseList = courses.get(size);
-            Course course = new Course(selectedMenus.stream()
-                    .sorted()
-                    .collect(Collectors.joining("")),
-                    occurrences);
+        return new ArrayList<String>(Arrays.asList(operands));
+    }
 
-            Course original = courseList.get(0);
-            if (original.occurrences < occurrences) {
-                courseList.clear();
-                courseList.add(course);
-            } else if (original.occurrences == occurrences) {
-                courseList.add(course);
+    private List<String> getOperators(String expression){
+        String[] operators = expression.split(operatorExpression);
+
+        return Arrays.stream(operators)
+                .filter(item -> !item.equals(""))
+                .collect(Collectors.toList());
+    }
+
+    private List<String> getFormula(List<String> operands, List<String> operators){
+        List<String> formula = new ArrayList<>();
+
+        for(int i=0; i < operands.size(); i++){
+            formula.add(operands.get(i));
+            if(i != operands.size()-1){
+                formula.add(operators.get(i));
             }
         }
 
-        if (size >= 10) return;
-        for (char menuChar = nextMenu; menuChar <= 'Z'; menuChar++) {
-            String menu = String.valueOf(menuChar);
-            selectedMenus.add(menu);
-            getCourses((char) (menuChar + 1), selectedMenus, orderList,
-                    courses);
-            selectedMenus.remove(menu);
-        }
+        return formula;
     }
 
-    public String[] solution(String[] orders, int[] course) {
-        List<Set<String>> orderList = Arrays.stream(orders)
-                .map(String::chars)
-                .map(charStream -> charStream
-                        .mapToObj(menu -> String.valueOf((char) menu))
-                        .collect(Collectors.toSet()))
-                .collect(Collectors.toList());
+    public List<String> toPostfix(List<String> formula, int i){
+        List<String> postfix = new ArrayList<>();
+        Deque<String> stack = new ArrayDeque<>();
 
-        Map<Integer, List<Course>> courses = new HashMap<>();
-        for (int length : course) {
-            List<Course> list = new ArrayList<>();
-            list.add(new Course("", 0));
-            courses.put(length, list);
+        for(String item : formula){
+            switch(item){
+                case "*":
+                    pushStack(stack, postfix, item, i);
+                    break;
+                case "+":
+                    pushStack(stack, postfix, item, i);
+                    break;
+                case "-":
+                    pushStack(stack, postfix, item, i);
+                    break;
+                default:
+                    postfix.add(item);
+            }
         }
-        getCourses('A', new HashSet<>(), orderList, courses);
 
-        return courses.values().stream()
-                .filter(list -> list.get(0).occurrences > 0)
-                .flatMap(List::stream)
-                .map(c -> c.course)
-                .sorted()
-                .toArray(String[]::new);
+        while(!stack.isEmpty()){
+            postfix.add(stack.removeLast());
+        }
+
+        return postfix;
     }
+
+    private long calculate(List<String> postfix){
+        Deque<Long> stack = new ArrayDeque<>();
+        long num1 = 0;
+        long num2 = 0;
+        long temp = 0;
+
+        for(String item : postfix){
+            switch(item){
+                case "*":
+                    num2 = stack.removeLast();
+                    num1 = stack.removeLast();
+                    temp = num1 * num2;
+                    stack.add(temp);
+                    break;
+                case "+":
+                    num2 = stack.removeLast();
+                    num1 = stack.removeLast();
+                    temp = num1 + num2;
+                    stack.add(temp);
+                    break;
+                case "-":
+                    num2 = stack.removeLast();
+                    num1 = stack.removeLast();
+                    temp = num1 - num2;
+                    stack.add(temp);
+                    break;
+                default:
+                    stack.add(Long.parseLong(item));
+            }
+        }
+
+        return stack.removeLast();
+    }
+
+    private void pushStack(Deque<String> stack, List<String> postfix, String item, int i){
+        while (!stack.isEmpty() && getPriority(stack.peekLast(), i) <= getPriority(item, i)) {
+            postfix.add(stack.removeLast());
+        }
+        stack.add(item);
+    }
+
+    private int getPriority(String operator, int i){
+        for(int j = 0; j < operatorPriority[i].length; j++){
+            if(operatorPriority[i][j].equals(operator)){
+                return j;
+            }
+        }
+        return 1;
+    }
+
 }
