5f27232923c8a0c1adab9d3ca16240f325863490
charlesuu <tiger0192004@gmail.com>
2023-08-21 23:52:14 +0900

    알고리즘

--- module1/src/main/java/org/example/Main.java
+++ module1/src/main/java/org/example/Main.java
@@ -1,31 +1,66 @@
 package org.example;
 
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
 
-    public static void main(String[] args) {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final int[] dx = {-1, 1, 0, 0};
+    private static final int[] dy = {0, 0, -1, 1};
+    private static final String VISITED = "X";
+    private static int n;
 
+    public static void dfs(int x, int y, String[][] board) {
+        String color = board[x][y];
+        board[x][y] = VISITED;
+        int nx, ny;
+
+        for (int i = 0; i < 4; i++) {
+            nx = x + dx[i];
+            ny = y + dy[i];
+
+            if ((0 <= nx && nx < n) && (0 <= ny && ny < n) && board[nx][ny].equals(color)) {
+                dfs(nx, ny, board);
+            }
+        }
     }
 
-    private List<int[]> hanoi(int n, int from, int to) {
-        if (n == 1) {
-            return List.of(new int[]{from, to});
+    public static void main(String[] args) throws IOException {
+        n = Integer.parseInt(br.readLine());
+        String[][] board1 = new String[n][n];
+        String[][] board2 = new String[n][n];
+        int result1 = 0;
+        int result2 = 0;
+        String color;
+
+        for (int i = 0; i < n; i++) {
+            String[] tokens = br.readLine().split("");
+
+            for (int j = 0; j < n; j++) {
+                color = tokens[j];
+                board1[i][j] = color;
+                board2[i][j] = color.equals("G") ? "R" : color;
+            }
         }
 
-        int empty = 6 - from - to;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!board1[i][j].equals(VISITED)) {
+                    dfs(i, j, board1);
+                    result1 += 1;
+                }
 
-        List<int[]> result = new ArrayList<>();
-        result.addAll(hanoi(n - 1, from, empty));
-        result.addAll(hanoi(1, from, to));
-        result.addAll(hanoi(n - 1, empty, to));
-        return result;
-    }
+                if (!board2[i][j].equals(VISITED)) {
+                    dfs(i, j, board2);
+                    result2 += 1;
+                }
+            }
+        }
 
-    public int[][] solution(int n) {
-        return hanoi(n, 1, 3).toArray(new int[0][]);
+        System.out.println(result1 + " " + result2);
     }
 }
