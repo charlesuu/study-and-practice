1c9e0584ef177bac3bb91cd22a53c1583f06e78d
charlesuu <tiger0192004@gmail.com>
2023-08-27 17:57:39 +0900

    알고리즘

--- module1/src/main/java/org/example/Main.java
+++ module1/src/main/java/org/example/Main.java
@@ -1,75 +1,30 @@
 package org.example;
 
 
-import java.util.Arrays;
+import java.util.Stack;
 
 public class Main {
 
-    private int scoreDiff(int[] apeach, int[] ryan) {
-        int diff = 0;
-        for (int i = 0; i < apeach.length; i++) {
-            if (apeach[i] == 0 && ryan[i] == 0) {
-                continue;
-            }
+    public boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
 
-            if (apeach[i] >= ryan[i]) {
-                diff -= 10 - i;
-            } else {
-                diff += 10 - i;
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                case '(' -> stack.push(c);
+                case ')' -> {
+                    if (stack.isEmpty()) {
+                        return false;
+                    }
+                    stack.pop();
+                }
             }
         }
-        return diff;
+
+        return stack.isEmpty();
     }
 
-    private boolean isPrior(int[] base, int[] comp) {
-        for (int i = 10; i >= 0; i--) {
-            if (comp[i] == base[i]) {
-                continue;
-            }
-            return comp[i] > base[i];
-        }
-        return false;
+    public static void main(String[] args) {
+        
     }
-
-    private int[] ryan(int index, int[] hits, int n, int[] apeach) {
-        if (index == hits.length) {
-            if (n > 0) {
-                return null;
-            }
-            if (scoreDiff(apeach, hits) <= 0) {
-                return null;
-            }
-            return Arrays.copyOf(hits, hits.length);
-        }
-
-        int maxDiff = 0;
-        int[] result = null;
-
-        for (int hit = 0; hit <= n; hit++) {
-            hits[index] = hit;
-            int[] ryan = ryan(index + 1, hits, n - hit, apeach);
-            if (ryan == null) {
-                continue;
-            }
-
-            int diff = scoreDiff(apeach, ryan);
-            if (diff > maxDiff ||
-                    (diff == maxDiff && isPrior(result, ryan))) {
-                maxDiff = diff;
-                result = ryan;
-            }
-        }
-
-        return result;
-    }
-
-    public int[] solution(int n, int[] info) {
-        int[] ryan = ryan(0, new int[11], n, info);
-        if (ryan == null) {
-            return new int[]{-1};
-        }
-        return ryan;
-    }
-
 
 }
