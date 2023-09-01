896e4ee37e43de4cda817b3f745eba7ed5c02b7f
charlesuu <tiger0192004@gmail.com>
2023-09-01 23:26:43 +0900

    알고리즘

--- module1/src/main/java/org/example/Main.java
+++ module1/src/main/java/org/example/Main.java
@@ -2,14 +2,53 @@ package org.example;
 
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Main {
 
-    public int solution(String skill, String[] skillTrees) {
-        return (int) Arrays.stream(skillTrees)
-                .map(s -> s.replaceAll("[^" + skill + "]", ""))
-                .filter(skill::startsWith)
-                .count();
+    private static class Job {
+
+        public final int start;
+        public final int duration;
+
+        private Job(int start, int duration) {
+            this.start = start;
+            this.duration = duration;
+        }
+    }
+
+    public int solution(int[][] rawJobs) {
+        Job[] jobs = new Job[rawJobs.length];
+        for (int i = 0; i < jobs.length; i++) {
+            jobs[i] = new Job(rawJobs[i][0], rawJobs[i][1]);
+        }
+        Arrays.sort(jobs, Comparator.comparingInt(job -> job.start));
+
+        Queue<Job> q = new LinkedList<>(Arrays.asList(jobs));
+        PriorityQueue<Job> pq = new PriorityQueue<>(
+                Comparator.comparingInt(job -> job.duration));
+
+        int exec = 0;
+        int time = 0;
+        while (!q.isEmpty() || !pq.isEmpty()) {
+            while (!q.isEmpty() && q.peek().start <= time) {
+                pq.add(q.poll());
+            }
+
+            if (pq.isEmpty()) {
+                time = q.peek().start;
+                continue;
+            }
+
+            Job job = pq.poll();
+            exec += time + job.duration - job.start;
+            time += job.duration;
+        }
+
+        return exec / jobs.length;
     }
 
     public static void main(String[] args) {
