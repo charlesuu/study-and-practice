cc52704dc8249b5ba83ae01f0958296bfe54ab5f
charlesuu <tiger0192004@gmail.com>
2023-09-18 14:30:19 +0900

    알고리즘

--- module1/src/main/java/org/example/Main.java
+++ module1/src/main/java/org/example/Main.java
@@ -4,74 +4,39 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static boolean[] switchs;
-    private static int N;
+    public static int H;
+    public static int W;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        N = sc.nextInt();
-        switchs = new boolean[N + 1];
+        H = sc.nextInt();
+        W = sc.nextInt();
 
-        for (int i = 1; i <= N; i++) {
-            switchs[i] = (sc.nextInt() == 1) ? true : false;
+        int[] wallHeights = new int[W];
+
+        for (int i = 0; i < W; i++) {
+            wallHeights[i] = sc.nextInt();
         }
 
-        int studentCount = sc.nextInt();
+        int result = 0;
+        for (int i = 1; i < W - 1; i++) { //i = pW
+            int leftMaxHeights = 0;
+            int rightMaxHeights = 0;
 
-        for (int i = 0; i < studentCount; i++) {
-            if (sc.nextInt() == 1) {
-                male(sc.nextInt());
-            } else {
-                female(sc.nextInt());
-            }
-        }
-
-        System.out.println(printSwitchs());
-    }
-
-    private static String printSwitchs() {
-        StringBuilder sb = new StringBuilder();
-
-        int count = 0;
-        for (int i = 1; i <= N; i++) {
-            if (switchs[i] == true) {
-                sb.append(1);
-            } else {
-                sb.append(0);
-            }
-            sb.append(" ");
-            count++;
-
-            if (count % 20 == 0) {
-                sb.append("\n");
-            }
-        }
-
-        return sb.toString();
-    }
-
-    private static void male(int switchNum) {
-        for (int i = switchNum; i <= N; i += switchNum) {
-            switchs[i] = !switchs[i];
-        }
-    }
-
-    private static void female(int switchNum) {
-        switchs[switchNum] = !switchs[switchNum];
-
-        int preNum = switchNum - 1;
-        int postNum = switchNum + 1;
-        while (true) {
-            if ((1 <= preNum && postNum <= N) && (switchs[preNum] == switchs[postNum])) {
-                switchs[preNum] = !switchs[preNum];
-                switchs[postNum] = !switchs[postNum];
-            } else {
-                break;
+            for (int j = 0; j < i; j++) {// 좌측에서 가장 높은 벽
+                leftMaxHeights = Math.max(leftMaxHeights, wallHeights[j]);
             }
 
-            preNum = preNum - 1;
-            postNum = postNum + 1;
+            for (int j = i + 1; j < W; j++) {// 우측에서 가장 높은 벽
+                rightMaxHeights = Math.max(rightMaxHeights, wallHeights[j]);
+            }
+
+            if (wallHeights[i] < leftMaxHeights && wallHeights[i] < rightMaxHeights) {//좌우측 가장 높은 벽보다 현 위치의 벽이 작은 경우
+                result +=
+                    Math.min(leftMaxHeights, rightMaxHeights) - wallHeights[i];//result += 좌우 벽 중 작은 벽의 높이 - 현 위치 벽의 높이
+            }
         }
+        System.out.println(result);
     }
 }
