8aae732b28b3206bbb7822848ac571859640484b
charlesuu <tiger0192004@gmail.com>
2023-10-02 21:15:15 +0900

    알고리즘

--- module1/src/main/java/org/example/Main.java
+++ module1/src/main/java/org/example/Main.java
@@ -1,74 +1,65 @@
 package org.example;
 
-import java.util.PriorityQueue;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
 
-    private static class DoublyPriorityQueue {
+    private static class State {
 
-        private int size = 0;
-        private final PriorityQueue<Integer> minPq
-            = new PriorityQueue<>();
-        private final PriorityQueue<Integer> maxPq
-            = new PriorityQueue<>((a, b) -> b - a);
+        public final int x;
+        public final int y;
+        public final int step;
 
-        public void add(int value) {
-            minPq.add(value);
-            maxPq.add(value);
-            size++;
-        }
-
-        public void removeMax() {
-            if (size == 0)
-                return;
-            maxPq.poll();
-            if (--size == 0) {
-                maxPq.clear();
-                minPq.clear();
-            }
-        }
-
-        public void removeMin() {
-            if (size == 0)
-                return;
-            minPq.poll();
-            if (--size == 0) {
-                maxPq.clear();
-                minPq.clear();
-            }
-        }
-
-        public int max() {
-            if (size == 0)
-                return 0;
-            return maxPq.peek();
-        }
-
-        public int min() {
-            if (size == 0)
-                return 0;
-            return minPq.peek();
+        private State(int x, int y, int step) {
+            this.x = x;
+            this.y = y;
+            this.step = step;
         }
     }
 
-    public int[] solution(String[] operations) {
-        DoublyPriorityQueue dpq = new DoublyPriorityQueue();
-        for (String operation : operations) {
-            String[] tokens = operation.split(" ");
-            String command = tokens[0];
-            String value = tokens[1];
-            switch (command) {
-                case "I" -> dpq.add(Integer.parseInt(value));
-                case "D" -> {
-                    if (value.equals("1")) {
-                        dpq.removeMax();
-                    } else {
-                        dpq.removeMin();
-                    }
+    private static final int[] dx = {0, 1, 0, -1};
+    private static final int[] dy = {-1, 0, 1, 0};
+
+    public int solution(int[][] maps) {
+        boolean[][] isVisited
+            = new boolean[maps.length][maps[0].length];
+
+        Queue<State> queue = new LinkedList<>();
+        queue.add(new State(0, 0, 1));
+        isVisited[0][0] = true;
+
+        while (!queue.isEmpty()) {
+            State state = queue.poll();
+
+            if (state.y == maps.length - 1 &&
+                state.x == maps[state.y].length - 1) {
+                return state.step;
+            }
+
+            for (int d = 0; d < 4; d++) {
+                int nx = state.x + dx[d];
+                int ny = state.y + dy[d];
+
+                if (ny < 0 || ny >= maps.length ||
+                    nx < 0 || nx >= maps[ny].length) {
+                    continue;
                 }
+
+                if (maps[ny][nx] != 1) {
+                    continue;
+                }
+
+                if (isVisited[ny][nx]) {
+                    continue;
+                }
+
+                isVisited[ny][nx] = true;
+                queue.add(new State(nx, ny, state.step + 1));
             }
         }
-        return new int[] {dpq.max(), dpq.min()};
+
+        return -1;
     }
 
     public static void main(String[] args) {
