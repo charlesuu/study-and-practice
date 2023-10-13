41bdd11c1948e720ab698f482ae00aa1e3792861
charlesuu <tiger0192004@gmail.com>
2023-10-13 21:38:00 +0900

    알고리즘

--- module1/src/main/java/org/example/Main.java
+++ module1/src/main/java/org/example/Main.java
@@ -1,8 +1,8 @@
 package org.example;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 
@@ -13,21 +13,22 @@ public class Main {
 
     }
 
-    public String solution(String[] participant, String[] completion) {
-        Map<String, Integer> count = new HashMap<>();
+    public double getSlope(int x1, int y1, int x2, int y2) {
+        return (double)(y2 - y1) / (x2 - x1);
+    }
 
-        for (String name : participant) {
-            count.putIfAbsent(name, 0);
-            count.put(name, count.get(name) + 1);
+    public int solution(int[][] dots) {
+        Set<Double> slopes = new HashSet<>();
+        for (int i = 0; i < dots.length; i++) {
+            for (int j = i + 1; j < dots.length; j++) {
+                double slope = getSlope(dots[i][0], dots[i][1],
+                        dots[j][0], dots[j][1]);
+                if (slopes.contains(slope)) {
+                    return 1;
+                }
+                slopes.add(slope);
+            }
         }
-
-        for (String name : completion) {
-            int v = count.get(name) - 1;
-            count.put(name, v);
-            if (v == 0)
-                count.remove(name);
-        }
-
-        return count.keySet().iterator().next();
+        return 0;
     }
 }
