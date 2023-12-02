65b2872c1eef9ce1c7c85e027b5817eeff97a710
charlesuu <tiger0192004@gmail.com>
2023-12-02 23:25:08 +0900

    알고리즘

--- module1/src/main/java/org/example/Main.java
+++ module1/src/main/java/org/example/Main.java
@@ -1,89 +1,27 @@
 package org.example;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.PriorityQueue;
 import java.util.stream.Collectors;
 
 public class Main {
 
-    private static class Course {
-
-        public final String course;
-        public final int occurrences;
-
-        public Course(String course, int occurrences) {
-            this.course = course;
-            this.occurrences = occurrences;
-        }
-    }
-
     public static void main(String[] args) {
-        
+
     }
 
-    private void getCourses(char nextMenu, Set<String> selectedMenus,
-            List<Set<String>> orderList,
-            Map<Integer, List<Course>> courses) {
-        int occurrences = (int)orderList.stream()
-                .filter(order -> order.containsAll(selectedMenus))
-                .count();
-        if (occurrences < 2)
-            return;
+    public int solution(int[] ability, int number) {
+        PriorityQueue<Integer> pq = Arrays.stream(ability)
+                .boxed()
+                .collect(Collectors.toCollection(PriorityQueue::new));
 
-        int size = selectedMenus.size();
-        if (courses.containsKey(size)) {
-            List<Course> courseList = courses.get(size);
-            Course course = new Course(selectedMenus.stream()
-                    .sorted()
-                    .collect(Collectors.joining("")),
-                    occurrences);
-
-            Course original = courseList.get(0);
-            if (original.occurrences < occurrences) {
-                courseList.clear();
-                courseList.add(course);
-            } else if (original.occurrences == occurrences) {
-                courseList.add(course);
-            }
+        for (int i = 0; i < number; i++) {
+            int a = pq.poll();
+            int b = pq.poll();
+            pq.add(a + b);
+            pq.add(a + b);
         }
 
-        if (size >= 10)
-            return;
-        for (char menuChar = nextMenu; menuChar <= 'Z'; menuChar++) {
-            String menu = String.valueOf(menuChar);
-            selectedMenus.add(menu);
-            getCourses((char)(menuChar + 1), selectedMenus, orderList,
-                    courses);
-            selectedMenus.remove(menu);
-        }
-    }
-
-    public String[] solution(String[] orders, int[] course) {
-        List<Set<String>> orderList = Arrays.stream(orders)
-                .map(String::chars)
-                .map(charStream -> charStream
-                        .mapToObj(menu -> String.valueOf((char)menu))
-                        .collect(Collectors.toSet()))
-                .collect(Collectors.toList());
-
-        Map<Integer, List<Course>> courses = new HashMap<>();
-        for (int length : course) {
-            List<Course> list = new ArrayList<>();
-            list.add(new Course("", 0));
-            courses.put(length, list);
-        }
-        getCourses('A', new HashSet<>(), orderList, courses);
-
-        return courses.values().stream()
-                .filter(list -> list.get(0).occurrences > 0)
-                .flatMap(List::stream)
-                .map(c -> c.course)
-                .sorted()
-                .toArray(String[]::new);
+        return pq.stream().reduce(0, Integer::sum);
     }
 }
