73a4d78a8784c2a7831d0d0330de29647b18315f
charlesuu <tiger0192004@gmail.com>
2023-12-08 18:21:54 +0900

    알고리즘

--- module1/src/main/java/org/example/Main.java
+++ module1/src/main/java/org/example/Main.java
@@ -6,28 +6,18 @@ public class Main {
 
     }
 
-    private boolean isValid(long t, int n, int[] times) {
-        long c = 0;
-        for (int time : times) {
-            c += t / time;
-        }
-        return c >= n;
-    }
+    public int[] solution(int brown, int yellow) {
+        for (int width = 3; width <= 5000; width++) {
+            for (int height = 3; height <= width; height++) {
+                int boundary = (width + height - 2) * 2;
+                int center = width * height - boundary;
 
-    public long solution(int n, int[] times) {
-        long start = 1;
-        long end = 1000000000000000000L;
-
-        while (end > start) {
-            long t = (start + end) / 2;
-
-            if (isValid(t, n, times)) {
-                end = t;
-            } else {
-                start = t + 1;
+                if (brown == boundary && yellow == center) {
+                    return new int[] {width, height};
+                }
             }
         }
 
-        return start;
+        return null;
     }
 }
