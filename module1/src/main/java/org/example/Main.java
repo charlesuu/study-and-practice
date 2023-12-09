49b434c25255f8764142b6143b8c6bfd7118b6e9
charlesuu <tiger0192004@gmail.com>
2023-12-09 22:04:10 +0900

    알고리즘

--- module1/src/main/java/org/example/Main.java
+++ module1/src/main/java/org/example/Main.java
@@ -1,23 +1,29 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
 
     }
 
-    public int[] solution(int brown, int yellow) {
-        for (int width = 3; width <= 5000; width++) {
-            for (int height = 3; height <= width; height++) {
-                int boundary = (width + height - 2) * 2;
-                int center = width * height - boundary;
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> count = new HashMap<>();
 
-                if (brown == boundary && yellow == center) {
-                    return new int[] {width, height};
-                }
-            }
+        for (String name : participant) {
+            count.putIfAbsent(name, 0);
+            count.put(name, count.get(name) + 1);
         }
 
-        return null;
+        for (String name : completion) {
+            int v = count.get(name) - 1;
+            count.put(name, v);
+            if (v == 0)
+                count.remove(name);
+        }
+
+        return count.keySet().iterator().next();
     }
 }
