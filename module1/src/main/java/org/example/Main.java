b6025863b70f4cd5f33d2b73beb579e6caf791e8
charlesuu <tiger0192004@gmail.com>
2023-12-16 15:37:10 +0900

    알고리즘

--- module1/src/main/java/org/example/Main.java
+++ module1/src/main/java/org/example/Main.java
@@ -1,8 +1,11 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -10,83 +13,51 @@ public class Main {
 
     }
 
-    private static class State {
-        public final int x;
-        public final int y;
-        public final int canJump;
-        public final int step;
+    private static class Program {
+        public final int priority;
+        public final int calledAt;
+        public final int executionTime;
 
-        public State(int x, int y, int canJump, int step) {
-            this.x = x;
-            this.y = y;
-            this.canJump = canJump;
-            this.step = step;
+        public Program(int priority, int calledAt, int executionTime) {
+            this.priority = priority;
+            this.calledAt = calledAt;
+            this.executionTime = executionTime;
         }
     }
 
-    private static final int[] dx = {0, 0, -1, 1};
-    private static final int[] dy = {-1, 1, 0, 0};
+    public long[] solution(int[][] program) {
+        Queue<Program> programs = Arrays.stream(program)
+                .map(p -> new Program(p[0], p[1], p[2]))
+                .sorted(Comparator.comparingInt(p -> p.calledAt))
+                .collect(Collectors.toCollection(LinkedList::new));
 
-    public int solution(int n, int m, int[][] hole) {
-        boolean[][] map = new boolean[m][n];
-        for (boolean[] row : map) {
-            Arrays.fill(row, true);
-        }
-        for (int[] h : hole) {
-            int x = h[0] - 1;
-            int y = h[1] - 1;
-            map[y][x] = false;
-        }
+        PriorityQueue<Program> pq = new PriorityQueue<>((a, b) -> {
+            if (a.priority != b.priority) {
+                return a.priority - b.priority;
+            }
+            return a.calledAt - b.calledAt;
+        });
 
-        boolean[][][] isVisited = new boolean[m][n][2];
+        long[] waitTimes = new long[11];
 
-        Queue<State> q = new LinkedList<>();
-        q.add(new State(0, 0, 1, 0));
-        isVisited[0][0][1] = true;
-
-        while (!q.isEmpty()) {
-            State s = q.poll();
-
-            if (s.x == n - 1 && s.y == m - 1) {
-                return s.step;
+        int time = 0;
+        while (!programs.isEmpty() || !pq.isEmpty()) {
+            while (!programs.isEmpty() &&
+                    time >= programs.peek().calledAt) {
+                pq.add(programs.poll());
             }
 
-            for (int d = 0; d < 4; d++) {
-                int nx = s.x + dx[d];
-                int ny = s.y + dy[d];
-
-                if (ny < 0 || ny >= m || nx < 0 || nx >= n) {
-                    continue;
-                }
-                if (map[ny][nx]) {
-                    if (isVisited[ny][nx][s.canJump]) {
-                        continue;
-                    }
-                    isVisited[ny][nx][s.canJump] = true;
-                    q.add(new State(nx, ny, s.canJump, s.step + 1));
-                }
-
-                if (s.canJump != 1) {
-                    continue;
-                }
-
-                int nnx = nx + dx[d];
-                int nny = ny + dy[d];
-
-                if (nny < 0 || nny >= m || nnx < 0 || nnx >= n) {
-                    continue;
-                }
-                if (!map[nny][nnx]) {
-                    continue;
-                }
-                if (isVisited[nny][nnx][0]) {
-                    continue;
-                }
-                isVisited[ny][nx][0] = true;
-                q.add(new State(nnx, nny, 0, s.step + 1));
+            if (pq.isEmpty()) {
+                time = programs.peek().calledAt;
+                continue;
             }
-        }
 
-        return -1;
+            Program p = pq.poll();
+            waitTimes[p.priority] += time - p.calledAt;
+            time += p.executionTime;
+        }
+        waitTimes[0] = time;
+
+        return waitTimes;
     }
 }
