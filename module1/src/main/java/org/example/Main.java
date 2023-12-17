7877cee3c2017a0a69576f27ba7af34203b6c081
charlesuu <tiger0192004@gmail.com>
2023-12-17 22:50:47 +0900

    알고리즘

--- module1/src/main/java/org/example/Main.java
+++ module1/src/main/java/org/example/Main.java
@@ -1,11 +1,6 @@
 package org.example;
 
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.stream.Collectors;
 
 public class Main {
 
@@ -13,51 +8,23 @@ public class Main {
 
     }
 
-    private static class Program {
-        public final int priority;
-        public final int calledAt;
-        public final int executionTime;
+    private String find(int n, int p) {
+        if (n == 1)
+            return "Rr";
 
-        public Program(int priority, int calledAt, int executionTime) {
-            this.priority = priority;
-            this.calledAt = calledAt;
-            this.executionTime = executionTime;
-        }
+        int slice = (int)Math.pow(4, n - 2);
+        int group = p / slice;
+
+        if (group == 0)
+            return "RR";
+        if (group == 3)
+            return "rr";
+        return find(n - 1, p % slice);
     }
 
-    public long[] solution(int[][] program) {
-        Queue<Program> programs = Arrays.stream(program)
-                .map(p -> new Program(p[0], p[1], p[2]))
-                .sorted(Comparator.comparingInt(p -> p.calledAt))
-                .collect(Collectors.toCollection(LinkedList::new));
-
-        PriorityQueue<Program> pq = new PriorityQueue<>((a, b) -> {
-            if (a.priority != b.priority) {
-                return a.priority - b.priority;
-            }
-            return a.calledAt - b.calledAt;
-        });
-
-        long[] waitTimes = new long[11];
-
-        int time = 0;
-        while (!programs.isEmpty() || !pq.isEmpty()) {
-            while (!programs.isEmpty() &&
-                    time >= programs.peek().calledAt) {
-                pq.add(programs.poll());
-            }
-
-            if (pq.isEmpty()) {
-                time = programs.peek().calledAt;
-                continue;
-            }
-
-            Program p = pq.poll();
-            waitTimes[p.priority] += time - p.calledAt;
-            time += p.executionTime;
-        }
-        waitTimes[0] = time;
-
-        return waitTimes;
+    public String[] solution(int[][] queries) {
+        return Arrays.stream(queries)
+                .map(query -> find(query[0], query[1] - 1))
+                .toArray(String[]::new);
     }
 }
