73f3a650f59f5b15100742c6b4b93ebb47a75d14
charlesuu <tiger0192004@gmail.com>
2023-12-18 17:38:11 +0900

    알고리즘

--- module1/src/main/java/org/example/Main.java
+++ module1/src/main/java/org/example/Main.java
@@ -1,30 +1,46 @@
 package org.example;
 
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
 
     }
 
-    private String find(int n, int p) {
-        if (n == 1)
-            return "Rr";
+    private static class Order {
+        public final int time;
+        public final int duration;
 
-        int slice = (int)Math.pow(4, n - 2);
-        int group = p / slice;
-
-        if (group == 0)
-            return "RR";
-        if (group == 3)
-            return "rr";
-        return find(n - 1, p % slice);
+        public Order(int time, int duration) {
+            this.time = time;
+            this.duration = duration;
+        }
     }
 
-    public String[] solution(int[][] queries) {
-        return Arrays.stream(queries)
-                .map(query -> find(query[0], query[1] - 1))
-                .toArray(String[]::new);
+    public int solution(int[] menu, int[] order, int k) {
+        Order[] orders = new Order[order.length];
+        for (int i = 0; i < order.length; i++) {
+            orders[i] = new Order(k * i, menu[order[i]]);
+        }
+
+        int max = 0;
+
+        int time = 0;
+        int end = 0;
+        for (int start = 0; start < orders.length; start++) {
+            Order o = orders[start];
+            if (time < o.time) {
+                time = o.time;
+            }
+
+            time += o.duration;
+            while (end < orders.length && orders[end].time < time) {
+                end++;
+            }
+            int count = end - start;
+            if (count > max) {
+                max = count;
+            }
+        }
+        return max;
     }
 }
