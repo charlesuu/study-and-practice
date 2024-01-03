8a6e4c54f633e27e5f1188f17cf6641d0e09f0c1
charlesuu <tiger0192004@gmail.com>
2024-01-03 23:53:31 +0900

    알고리즘

--- module1/src/main/java/org/example/Main.java
+++ module1/src/main/java/org/example/Main.java
@@ -1,92 +1,24 @@
 package org.example;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class Main {
 
     public static void main(String[] args) {
 
     }
 
-    private static class State {
-        public final int x;
-        public final int y;
-        public final int canJump;
-        public final int step;
+    public String solution(String inputString) {
+        inputString = "." + inputString + ".";
 
-        public State(int x, int y, int canJump, int step) {
-            this.x = x;
-            this.y = y;
-            this.canJump = canJump;
-            this.step = step;
-        }
-    }
-
-    private static final int[] dx = {0, 0, -1, 1};
-    private static final int[] dy = {-1, 1, 0, 0};
-
-    public int solution(int n, int m, int[][] hole) {
-        boolean[][] map = new boolean[m][n];
-        for (boolean[] row : map) {
-            Arrays.fill(row, true);
-        }
-        for (int[] h : hole) {
-            int x = h[0] - 1;
-            int y = h[1] - 1;
-            map[y][x] = false;
-        }
-
-        boolean[][][] isVisited = new boolean[m][n][2];
-
-        Queue<State> q = new LinkedList<>();
-        q.add(new State(0, 0, 1, 0));
-        isVisited[0][0][1] = true;
-
-        while (!q.isEmpty()) {
-            State s = q.poll();
-
-            if (s.x == n - 1 && s.y == m - 1) {
-                return s.step;
+        StringBuilder builder = new StringBuilder();
+        for (char c = 'a'; c <= 'z'; c++) {
+            if (inputString.split(c + "+").length < 3) {
+                continue;
             }
-
-            for (int d = 0; d < 4; d++) {
-                int nx = s.x + dx[d];
-                int ny = s.y + dy[d];
-
-                if (ny < 0 || ny >= m || nx < 0 || nx >= n) {
-                    continue;
-                }
-                if (map[ny][nx]) {
-                    if (isVisited[ny][nx][s.canJump]) {
-                        continue;
-                    }
-                    isVisited[ny][nx][s.canJump] = true;
-                    q.add(new State(nx, ny, s.canJump, s.step + 1));
-                }
-
-                if (s.canJump != 1) {
-                    continue;
-                }
-
-                int nnx = nx + dx[d];
-                int nny = ny + dy[d];
-
-                if (nny < 0 || nny >= m || nnx < 0 || nnx >= n) {
-                    continue;
-                }
-                if (!map[nny][nnx]) {
-                    continue;
-                }
-                if (isVisited[nny][nnx][0]) {
-                    continue;
-                }
-                isVisited[ny][nx][0] = true;
-                q.add(new State(nnx, nny, 0, s.step + 1));
-            }
+            builder.append(c);
         }
 
-        return -1;
+        if (builder.length() == 0)
+            return "N";
+        return builder.toString();
     }
 }
