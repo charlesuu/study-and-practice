87b268d1574ef7511bcf67ae552d995b156aa9f5
이현호 <tiger0192004@gmail.com>
2024-02-03 23:33:53 +0900

    알고리즘

--- module1/src/main/java/org/example/Main.java
+++ module1/src/main/java/org/example/Main.java
@@ -1,44 +1,25 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
 
-	private static final int[] dx = {0, 1, -1};
-	private static final int[] dy = {1, 0, -1};
-
-	public int[] solution(int n) {
-		int[][] triangle = new int[n][n];
-		int v = 1;
-		int x = 0;
-		int y = 0;
-		int d = 0;
-
-		while (true) {
-			triangle[y][x] = v++;
-			int nx = x + dx[d];
-			int ny = y + dy[d];
-			if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0) {
-				d = (d + 1) % 3;
-				nx = x + dx[d];
-				ny = y + dy[d];
-				if (nx == n || ny == n || nx == -1 || ny == -1 || triangle[ny][nx] != 0)
-					break;
-			}
-			x = nx;
-			y = ny;
-		}
-
-		int[] result = new int[v - 1];
-		int index = 0;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j <= i; j++) {
-				result[index++] = triangle[i][j];
-			}
-		}
-
-		return result;
-	}
-
 	public static void main(String args[]) {
-
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] a = new int[n + 1];
+		for (int i = 1; i <= n; i++) {
+			a[i] = sc.nextInt();
+		}
+		int[] d = new int[n + 1];
+		for (int i = 1; i <= n; i++) {
+			d[i] = -1;
+			for (int j = 1; j <= i; j++) {
+				if (d[i] == -1 || d[i] > d[i - j] + a[j]) {
+					d[i] = d[i - j] + a[j];
+				}
+			}
+		}
+		System.out.println(d[n]);
 	}
 }
