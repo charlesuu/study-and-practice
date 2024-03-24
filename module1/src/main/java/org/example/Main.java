beba9ae6e2fbd3dc5ac7261108e9fd6db9e905a1
이현호 <tiger0192004@gmail.com>
2024-03-24 23:52:45 +0900

    알고리즘

--- module1/src/main/java/org/example/Main.java
+++ module1/src/main/java/org/example/Main.java
@@ -1,72 +1,49 @@
 package org.example;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int R = sc.nextInt();
 
-        int[][] a = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                a[i][j] = sc.nextInt();
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        int[] d = new int[n];
+        int[] dr = new int[n];
+        for (int i = 0; i < n; i++) {
+            d[i] = a[i];
+            if (i > 0 && d[i] < d[i - 1] + a[i]) {
+                d[i] = d[i - 1] + a[i];
+            }
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            dr[i] = a[i];
+            if (i < n - 1 && dr[i] < dr[i + 1] + a[i]) {
+                dr[i] = dr[i + 1] + a[i];
+            }
+
+        }
+        int ans = d[0];
+        for (int i = 1; i < n; i++) {
+            if (ans < d[i]) {
+                ans = d[i];
+            }
+        }
+        for (int i = 1; i < n - 1; i++) {
+            if (ans < d[i - 1] + dr[i + 1]) {
+                ans = d[i - 1] + dr[i + 1];
             }
         }
 
-        int groupCount = Math.min(N, M) / 2;
-        ArrayList<ArrayList<Integer>> groups = new ArrayList<>();
-        for (int k = 0; k < groupCount; k++) {
-            ArrayList<Integer> group = new ArrayList<>();
-            for (int j = k; j < M - k; j++) {
-                group.add(a[k][j]);
-            }
-            for (int i = k + 1; i < N - 1 - k; i++) {
-                group.add(a[i][M - 1 - k]);
-            }
-            for (int j = M - 1 - k; j > k; j--) {
-                group.add(a[N - 1 - k][j]);
-            }
-            for (int i = N - k - 1; i > k; i--) {
-                group.add(a[i][k]);
-            }
-            groups.add(group);
-        }
-
-        for (int k = 0; k < groupCount; k++) {
-            ArrayList<Integer> group = groups.get(k);
-            int n = group.size();
-            int movedIndex = R % n;
-
-            for (int j = k; j < M - k; j++) {
-                a[k][j] = group.get(movedIndex);
-                movedIndex = (movedIndex + 1) % n;
-            }
-            for (int i = k + 1; i < N - 1 - k; i++) {
-                a[i][M - 1 - k] = group.get(movedIndex);
-                movedIndex = (movedIndex + 1) % n;
-            }
-            for (int j = M - 1 - k; j > k; j--) {
-                a[N - 1 - k][j] = group.get(movedIndex);
-                movedIndex = (movedIndex + 1) % n;
-            }
-            for (int i = N - k - 1; i > k; i--) {
-                a[i][k] = group.get(movedIndex);
-                movedIndex = (movedIndex + 1) % n;
-            }
-        }
-
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                stringBuilder.append(a[i][j]).append(" ");
-            }
-            stringBuilder.append("\n");
-        }
-        System.out.println(stringBuilder);
+        System.out.println(ans);
+        
     }
 }
+
+
+
