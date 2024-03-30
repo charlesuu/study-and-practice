99513277147d30817c3d2b2484305ed9cac12a1b
이현호 <tiger0192004@gmail.com>
2024-03-30 23:12:23 +0900

    알고리즘

--- module1/src/main/java/org/example/Main.java
+++ module1/src/main/java/org/example/Main.java
@@ -1,100 +1,69 @@
 package org.example;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Main {
 
-    private static class Fee {
-        private final int baseTime;
-        private final int baseFee;
-        private final int unitTime;
-        private final int unitFee;
+    private static class Hand {
+        private final int baseX;
+        public final String hand;
+        public final float preference;
+        private int x;
+        private int y;
 
-        public Fee(int baseTime, int baseFee, int unitTime,
-                int unitFee) {
-            this.baseTime = baseTime;
-            this.baseFee = baseFee;
-            this.unitTime = unitTime;
-            this.unitFee = unitFee;
+        public Hand(String hand, boolean isPreferred, int x) {
+            this.hand = hand;
+            this.baseX = x;
+            this.preference = isPreferred ? 0.5f : 0;
+            this.x = x;
+            this.y = 3;
         }
 
-        public int cost(int time) {
-            int fee = baseFee;
-            time -= baseTime;
-
-            while (time > 0) {
-                fee += unitFee;
-                time -= unitTime;
-            }
-
-            return fee;
-        }
-    }
-
-    private static class Car {
-        public final String number;
-        private final Fee fee;
-        private int inTime = -1;
-        private int totalTime = 0;
-
-        public Car(String number, Fee fee) {
-            this.number = number;
-            this.fee = fee;
+        public float distance(int x, int y) {
+            if (x == baseX) return 0;
+            int distance = Math.abs(x - this.x) + Math.abs(y - this.y);
+            return distance - preference;
         }
 
-        public void in(int time) {
-            this.inTime = time;
-        }
-
-        public void out(int time) {
-            if (this.inTime == -1) return;
-            totalTime += time - this.inTime;
-            this.inTime = -1;
-        }
-
-        public int cost() {
-            return fee.cost(totalTime);
+        public void move(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
 
-    private int parseTime(String time) {
-        int hour = Integer.parseInt(time.substring(0, 2));
-        int minute = Integer.parseInt(time.substring(3));
-        return hour * 60 + minute;
+    private int getX(int number) {
+        if (number == 0) return 1;
+        return (number - 1) % 3;
     }
 
-    public int[] solution(int[] fees, String[] records) {
-        Fee fee = new Fee(fees[0], fees[1], fees[2], fees[3]);
+    private int getY(int number) {
+        if (number == 0) return 3;
+        return (number - 1) / 3;
+    }
 
-        Map<String, Car> cars = new HashMap<>();
-        for (String record : records) {
-            String[] tokens = record.split(" ");
-            int time = parseTime(tokens[0]);
-            String number = tokens[1];
-            boolean isIn = tokens[2].equals("IN");
+    private Hand press(int number, Hand right, Hand left) {
+        int x = getX(number);
+        int y = getY(number);
 
-            if (!cars.containsKey(number)) {
-                cars.put(number, new Car(number, fee));
-            }
-            Car car = cars.get(number);
-            if (isIn) {
-                car.in(time);
-            } else {
-                car.out(time);
-            }
+        float rDistance = right.distance(x, y);
+        float lDistance = left.distance(x, y);
+
+        Hand hand = right;
+        if (lDistance < rDistance) {
+            hand = left;
         }
+        hand.move(x, y);
+        return hand;
+    }
 
-        int endTime = parseTime("23:59");
-        for (Car car : cars.values()) {
-            car.out(endTime);
-        }
+    public String solution(int[] numbers, String hand) {
+        Hand right = new Hand("R", hand.equals("right"), 2);
+        Hand left = new Hand("L", hand.equals("left"), 0);
 
-        return cars.values().stream()
-                .sorted(Comparator.comparing(car -> car.number))
-                .mapToInt(Car::cost)
-                .toArray();
+        return Arrays.stream(numbers)
+                .mapToObj(n -> press(n, right, left).hand)
+                .collect(Collectors.joining());
     }
 
     public static void main(String[] args) {
